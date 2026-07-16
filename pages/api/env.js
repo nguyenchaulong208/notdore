6ee2f3c194c801bb08660e145f3ef91e2d1675a3
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2026-07-17 02:21:36 +0700

    .

--- pages/api/env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const url = process.env.SUPABASE_URL || '';
-  const key = process.env.SUPABASE_ANON_KEY || '';
-
-  res.setHeader('Cache-Control', 'no-cache');
-  res.status(200).json({ url, key });
-}
